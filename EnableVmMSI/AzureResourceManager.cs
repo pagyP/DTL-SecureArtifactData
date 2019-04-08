d1ed2f9029b04bbed532bdb0aa61b4e6b71f4a08
Roger Best <rbest@microsoft.com>
2019-04-08 16:13:34 -0700

    Updated to handle multiple labs, added comments, and logging

--- EnableVmMSI/AzureResourceManager.cs
+++ EnableVmMSI/AzureResourceManager.cs
@@ -10,6 +10,7 @@ using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
 using Microsoft.Azure.Management.ResourceManager.Fluent;
 using Microsoft.Azure.Management.Fluent;
 using Microsoft.Azure.Management.KeyVault.Fluent.Models;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Flurl.Http;
@@ -21,48 +22,47 @@ namespace EnableVmMSI
     public sealed class AzureResourceManager
     {
         
-        private KeyVaultClient _kv;        
-        //*private ClientCredential _clientCred;
-        //*private IAzure _azure;
+        private KeyVaultClient _kv;
         private IAzure _msiazure;
         private string _accessToken;
 
-        public AzureResourceManager(AzureResourceInformation resourceId, KeyVaultInformation kvInfo)
+        
+        public AzureResourceManager(AzureResourceInformation resourceId, KeyVaultInformation kvInfo, ILogger log)
         {
  
-            Initialize(resourceId, kvInfo).Wait();
-            AddIMSIToVMAsync(resourceId, kvInfo).Wait();
+            Initialize(resourceId, kvInfo, log).Wait();
+            if (!String.IsNullOrEmpty(resourceId.LabName))
+            {
+                AddIMSIToVMAsync(resourceId, kvInfo, log).Wait();
+            }
             
         }
-
-        private async Task Initialize(AzureResourceInformation resourceInfo, KeyVaultInformation vault)
+        /*
+         * Input: AzureResourceInformation, KeyVaultInformation, Logger
+         * Get the necessary credential information for VM management and KeyVault access.
+         */
+        private async Task Initialize(AzureResourceInformation resourceInfo, KeyVaultInformation vault, ILogger log)
         {
-
-            //AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+            
+            // Get the keyvault client
             var azureServiceTokenProvider = new AzureServiceTokenProvider();
             _kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
 
             _accessToken = await azureServiceTokenProvider.GetAccessTokenAsync("https://management.azure.com/");
 
-            //*string _id = (await _kv.GetSecretAsync(vault.KeyVaultUri, vault.KV_SecretName_ServicePrinciple)).Value;
-            //*string _cred = (await _kv.GetSecretAsync(vault.KeyVaultUri, vault.KV_SecretName_ServicePrinciplePwd)).Value;
-
+            // Get the LabResourceGroup
             resourceInfo.LabResourceGroup = ParseLabResourceGroup(resourceInfo.ResourceUri);
+            resourceInfo.LabName = await GetLabName(resourceInfo.LabResourceGroup);
 
-            //*AzureCredentials _azureCred = SdkContext.AzureCredentialsFactory.FromServicePrincipal(
-            //*    _id, _cred, resourceInfo.TenantId, AzureEnvironment.AzureGlobalCloud);
-
+            // Get the management credentials
             MSILoginInformation msiInfo = new MSILoginInformation(MSIResourceType.AppService);
             AzureCredentials _msiazureCred = SdkContext.AzureCredentialsFactory.FromMSI(msiInfo,AzureEnvironment.AzureGlobalCloud);
 
-            //*_azure = Azure.Authenticate(_azureCred).WithSubscription(resourceInfo.SubscriptionId);
-
             _msiazure = Azure.Authenticate(_msiazureCred).WithSubscription(resourceInfo.SubscriptionId);
-
-            //*_clientCred = new ClientCredential(_id, _cred);
 
         }
 
+        // Parse the Lab resource group from the resource id
         private string ParseLabResourceGroup(string resourceId)
         {
             int first = (resourceId.IndexOf("resourceGroups/") + 15);
@@ -70,7 +70,45 @@ namespace EnableVmMSI
 
         }
 
-        public async Task AddIMSIToVMAsync(AzureResourceInformation resourceInfo, KeyVaultInformation vault)
+        // Get the lab with the resource group
+        private async Task<string> GetLabName(string resourceGroup)
+        {
+            string[] expandProperty = new string[] { "api-version=2018-10-15-preview" };
+
+            var response = await new Url($"https://management.azure.com/subscriptions/da8f3095-ac12-4ef2-9b35-fcd24842e207/providers/Microsoft.DevTestLab/labs")
+                    .WithOAuthBearerToken(_accessToken)
+                    .SetQueryParams(expandProperty)
+                    .GetStringAsync();
+
+            JObject vmsObject = JObject.Parse(response);
+            JArray vms = (JArray)vmsObject.SelectToken("value");
+
+            foreach (JToken lab in vms.Children())
+            {
+
+                int first = 0;
+                string labName = "";
+
+                JToken rgId = lab.SelectToken("$.properties.vmCreationResourceGroupId");
+
+                if (rgId != null)
+                {
+                    first = (rgId.ToString().IndexOf("resourceGroups/") + 15);
+                    labName = rgId.ToString().Substring(first, rgId.ToString().IndexOf("/", first) - first);
+                    if (labName == resourceGroup)
+                    {
+                        return labName;
+                    }
+                }
+
+            }
+
+            return null;
+            
+        }
+
+        // Enable the IMSI on the Vm and add the IMSI id to the keyvault access policy
+        public async Task AddIMSIToVMAsync(AzureResourceInformation resourceInfo, KeyVaultInformation vault, ILogger log)
         {
             List<string> allVms = await GetArtifactInfoAsync(resourceInfo);
 
@@ -86,7 +124,23 @@ namespace EnableVmMSI
 
                             if (!vm.IsManagedServiceIdentityEnabled)
                             {
-                                await vm.Update().WithSystemAssignedManagedServiceIdentity().ApplyAsync();
+                                // Don't await this call as issue where hangs, handle manually below
+                                vm.Update().WithSystemAssignedManagedServiceIdentity().ApplyAsync();
+                                // Handle await manually.
+                                TimeSpan timeSpan = new TimeSpan(0, 0, 10);
+                                int counter = 0;
+                                await Task.Delay(timeSpan);
+                                while ((!vm.IsManagedServiceIdentityEnabled) || (String.IsNullOrEmpty(vm.SystemAssignedManagedServiceIdentityPrincipalId)))
+                                {
+                                    counter++;
+                                    await Task.Delay(timeSpan);
+                                    log.LogInformation("@@@@@@@@@@@@ HHHH:" + DateTime.Now.ToString());
+                                    vm.Refresh();
+                                    if (counter == 20)
+                                    {
+                                        break;
+                                    }
+                                }
 
                             }
 
@@ -98,20 +152,21 @@ namespace EnableVmMSI
                                 .Attach()
                                 .ApplyAsync();
                             // Remove after 30 min async
-                            RemoveAccess(vm, _keyVault);
+                            log.LogInformation("Execute removal.");
+                            await RemoveAccess(vm, _keyVault, log);
                         }
-                        catch (Exception e) { }
+                        catch (Exception e) {
+                            log.LogInformation("[Error] " + e.Message);
+                        }
                     }
                 }
             }
         }
 
-
+        // Determine the VM that the artifact is being applied to.
         private async Task<List<string>> GetArtifactInfoAsync(AzureResourceInformation resourceInfo)
         {
             List<string> computeId = new List<string>();
-            //*var context = new AuthenticationContext($"https://login.windows.net/{resourceInfo.TenantId}", false);
-            //*var token = await context.AcquireTokenAsync("https://management.azure.com/", _clientCred);
 
             string[] expandProperty = new string[] {"$expand=properties($expand=artifacts)", "api-version=2018-10-15-preview"};
 
@@ -137,14 +192,17 @@ namespace EnableVmMSI
             return computeId; 
         }
 
-        private async Task RemoveAccess(Microsoft.Azure.Management.Compute.Fluent.IVirtualMachine vm, Microsoft.Azure.Management.KeyVault.Fluent.IVault vault )
+        // Remove the IMSI from the VM and the KeyVault Access policy
+        private async Task RemoveAccess(Microsoft.Azure.Management.Compute.Fluent.IVirtualMachine vm, Microsoft.Azure.Management.KeyVault.Fluent.IVault vault, ILogger log)
         {
-            TimeSpan timeSpan = new TimeSpan(0, 30, 0);
+            TimeSpan timeSpan = new TimeSpan(0, 4, 0);
             await Task.Delay(timeSpan);
+            log.LogInformation("In removal");
             await vault.Update()
                 .WithoutAccessPolicy(vm.SystemAssignedManagedServiceIdentityPrincipalId).ApplyAsync();
-                    
+            log.LogInformation("Post policy");
             await vm.Update().WithoutSystemAssignedManagedServiceIdentity().ApplyAsync();
+            log.LogInformation("Post identity");
         }
     }
 
