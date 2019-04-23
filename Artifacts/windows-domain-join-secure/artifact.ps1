a2a88142811060d95c9fc513557fc85ef507b1ff
Roger Best <rbest@microsoft.com>
2019-04-23 14:17:31 -0700

    Add updated artifact

--- Artifacts/windows-domain-join-secure/artifact.ps1
+++ Artifacts/windows-domain-join-secure/artifact.ps1
@@ -44,53 +44,23 @@ trap
 # Functions used in this script.
 #
 
-function Join-Domain 
-{
-    [CmdletBinding()]
-    param
-    (
-        [string] $DomainName,
-        [string] $UserName,
-        [securestring] $Password
-    )
-
-    if ((Get-WmiObject Win32_ComputerSystem).Domain -eq $DomainName)
-    {
-        Write-Host "Computer $($Env:COMPUTERNAME) is already joined to domain $DomainName."
-    }
-    else
-    {
-        $credential = New-Object System.Management.Automation.PSCredential($UserName, $Password)
-        
-        if ($OUPath)
-        {
-            [Microsoft.PowerShell.Commands.ComputerChangeInfo]$computerChangeInfo = Add-Computer -DomainName $DomainName -Credential $credential -OUPath $OUPath -Force -PassThru
-        }
-        else
-        {
-            [Microsoft.PowerShell.Commands.ComputerChangeInfo]$computerChangeInfo = Add-Computer -DomainName $DomainName -Credential $credential -Force -PassThru
-        }
-        
-        if (-not $computerChangeInfo.HasSucceeded)
-        {
-            throw "Failed to join computer $($Env:COMPUTERNAME) to domain $DomainName."
-        }
-        
-        Write-Host "Computer $($Env:COMPUTERNAME) successfully joined domain $DomainName."
-    }
-}
 
 ###################################################################################################
 #
 # Main execution block.
 #
-$MaxRetries = 25
+$MaxRetries = 30
 $currentRetry = 0
 $success = $false
-$DomainToJoin = "corp.rbestlocaldom.us"
+$DomainToJoin = "mydomain.com"
 $KeyVaultName = "testkeyvault"
 
 Write-Host "Start: " + $(Get-Date)
+
+$DomainAdminPassword = $null
+$DomainAdminUsername = $null
+
+
 do {
     try
     {
@@ -98,7 +68,8 @@ do {
         {
             throw "The current version of PowerShell is $($PSVersionTable.PSVersion.Major). Prior to running this artifact, ensure you have PowerShell 3 or higher installed."
         }
-
+        
+        # Get KeyVault token        
         $response = Invoke-WebRequest -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -Method GET -Headers @{Metadata="true"} -UseBasicParsing
         Write-Host "Success: " + $(Get-Date)
         $content = $response.Content | ConvertFrom-Json
@@ -114,23 +85,64 @@ do {
         $result = (Invoke-WebRequest -Uri "https://$KeyVaultName.vault.azure.net/secrets/TestAccountUser?api-version=2016-10-01" -Method GET -Headers @{Authorization="Bearer $KeyVaultToken"} -UseBasicParsing).content
         $begin = $result.IndexOf("value") + 8
         $endlength = ($result.IndexOf('"',$begin) -10)
-        $DomainAdminUsername = $result.Substring($begin,$endlength)
+        $tempname = $result.Substring($begin,$endlength)
+        $DomainAdminUsername = $tempname.Replace("\\","\")
+        Write-Host "Account Name: $DomainAdminUsername"
 
-        Write-Host "Attempting to join computer $($Env:COMPUTERNAME) to domain $DomainToJoin."
-        $securePass = ConvertTo-SecureString $DomainAdminPassword -AsPlainText -Force
-        Join-Domain -DomainName $DomainToJoin -User $DomainAdminUsername -Password $securePass
-
-        Write-Host 'Artifact applied successfully.'
-        $success = $true
-        Pop-Location
+        if (($DomainAdminUsername -ne $null) -and ($DomainAdminPassword -ne $null)) {
+            $success = $true
+        }
+        else {
+            write-Host "KeyVault requests succeeded, but domain information is null."
+        }
     }
     catch {
         $currentRetry = $currentRetry + 1
         Write-Host "In catch $currentRetry $(Get-Date)"
         if ($currentRetry -gt $MaxRetries) {
-            throw "Failed Max retries"
+            #throw "Failed Max retries"
+            Write-Host "Failed Max retries"
+            break
         } else {
             Start-Sleep -Seconds 60
         }
-    }    
+    }
+    
 } while (!$success)
+
+# Execute the domain join
+if ($success) {
+    if (($DomainAdminUsername -ne $null) -and ($DomainAdminPassword -ne $null)) {
+
+        Write-Host "Attempting to join computer $($Env:COMPUTERNAME) to domain $DomainToJoin."
+        $securePass = ConvertTo-SecureString $DomainAdminPassword -AsPlainText -Force
+
+        if ((Get-WmiObject Win32_ComputerSystem).Domain -eq $DomainToJoin)
+            {
+                Write-Host "Computer $($Env:COMPUTERNAME) is already joined to domain $DomainToJoin."
+            }
+            else
+            {
+                
+                $credential = New-Object System.Management.Automation.PSCredential($DomainAdminUsername, $securePass)
+        
+                [Microsoft.PowerShell.Commands.ComputerChangeInfo]$computerChangeInfo = Add-Computer -DomainName $DomainToJoin -Credential $credential -Force -PassThru -Verbose
+        
+                if (-not $computerChangeInfo.HasSucceeded)
+                {
+                    throw "Failed to join computer $($Env:COMPUTERNAME) to domain $DomainToJoin."
+                }
+        
+                Write-Host "Computer $($Env:COMPUTERNAME) successfully joined domain $DomainToJoin."
+            }
+
+        Write-Host 'Artifact applied.'
+
+        }
+    else {
+        throw "Missing Domain join information."
+    }
+
+} else {
+    throw "Domain Join Artiffact failed to retrieve Domain information."
+}
