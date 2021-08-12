8b6121cf3854255a32682dd57b442d5da18b0bdc
Paul Paginton <paul.x.paginton@gsk.com>
2021-08-12 17:15:37 +0100

    remove format disk from vm disk creation script

--- Artifacts/new-vm-data-disk/artifact.ps1
+++ Artifacts/new-vm-data-disk/artifact.ps1
@@ -5,6 +5,10 @@ $storageType = 'StandardSSD_LRS'
 $dataDiskName = $vmName + '_datadisk1'
 $subscriptionID = '6d82d6cd-2bcc-4588-8db1-1e2c8c763f56'
 
+
+
+
+
 #$response = Invoke-WebRequest -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -Method GET -Headers @{Metadata="true"} -UseBasicParsing
  #       Write-Host "Success: " + $(Get-Date)
         Invoke-WebRequest -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -Method GET -Headers @{Metadata="true"} -UseBasicParsing
@@ -25,18 +29,3 @@ $vm = Add-AzVMDataDisk -VM $vm -Name $dataDiskName -CreateOption Attach -Managed
 Update-AzVM -VM $vm -ResourceGroupName $rgName
 
 #Start-Sleep -Seconds 30
-
-$disks = Get-Disk | Where-Object partitionstyle -eq 'raw' | Sort-Object number
-
-    $letters = 70..89 | ForEach-Object { [char]$_ }
-    $count = 0
-    $labels = "data1","data2"
-
-    foreach ($disk in $disks) {
-        $driveLetter = $letters[$count].ToString()
-        $disk |
-        Initialize-Disk -PartitionStyle MBR -PassThru |
-        New-Partition -UseMaximumSize -DriveLetter $driveLetter |
-        Format-Volume -FileSystem NTFS -NewFileSystemLabel $labels[$count] -Confirm:$false -Force
-	$count++
-    }
