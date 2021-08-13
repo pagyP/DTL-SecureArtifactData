6b6406682767d9baa808fc80469edef6c03d77ab
Paul Paginton <paul.x.paginton@gsk.com>
2021-08-13 10:46:17 +0100

    test2

--- Artifacts/new-vm-data-disk/artifact.ps1
+++ Artifacts/new-vm-data-disk/artifact.ps1
@@ -23,10 +23,10 @@ Select-AzSubscription -SubscriptionId $subscriptionID
 $diskConfig = New-AzDiskConfig -SkuName $storageType -Location $location -CreateOption Empty -DiskSizeGB 128 
 $dataDisk1 = New-AzDisk -DiskName $dataDiskName -Disk $diskConfig -ResourceGroupName $rgName 
 
-$vm = Get-AzVM -Name $vmName -ResourceGroupName $rgName
-$vm = Add-AzVMDataDisk -VM $vm -Name $dataDiskName -CreateOption Attach -ManagedDiskId $dataDisk1.Id -Lun 1
+#$vm = Get-AzVM -Name $vmName -ResourceGroupName $rgName
+ Add-AzVMDataDisk -VM $vmName -Name $dataDiskName -CreateOption Attach -ManagedDiskId $dataDisk1.Id -Lun 1
 
-# Update-AzVM -VM $vm -ResourceGroupName $rgName
+ Update-AzVM -VM $vm -ResourceGroupName $rgName
 # Get-ChildItem
 
 #Start-Sleep -Seconds 30
